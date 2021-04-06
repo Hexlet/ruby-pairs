44aa0d703f52da3eacb41b6556b141e2fdce161f
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2021-04-06 20:38:11 +0300

    fix gem structure

---
+++ lib/pairs.rb
@@ -0,0 +1,45 @@
+module Pairs
+  def self.cons(left, right)
+    lambda { |message|
+      case message
+      when 'car'
+        left
+      when 'cdr'
+        right
+      else
+        raise "Unknown message #{message}"
+      end
+    }
+  end
+
+  def self.check_pair(pair)
+    return if pair?(pair)
+
+    raise NoMethodError, pair.to_s
+  end
+
+  def self.car(pair)
+    check_pair(pair)
+    pair.call('car')
+  end
+
+  def self.cdr(pair)
+    pair.call('cdr')
+  end
+
+  def self.pair?(pair)
+    pair.instance_of? Proc
+  end
+
+  def self.to_string(pair)
+    rec = lambda { |pair_inner|
+      return pair_inner.to_s unless pair?(pair_inner)
+
+      left = car(pair_inner)
+      right = cdr(pair_inner)
+      "(#{rec.call(left)}, #{rec.call(right)})"
+    }
+
+    rec.call(pair)
+  end
+end
